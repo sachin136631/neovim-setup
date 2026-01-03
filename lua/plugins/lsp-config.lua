d296532c50e2410fc623f350eac46d3cf72866eb
sachin manoj <sachinmanojkr@gmail.com>
2026-01-04 00:34:33 +0530

    add auto complete snipet

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,31 +1,71 @@
 return {
-  {"williamboman/mason.nvim",
-  config=function()
-    require("mason").setup()
-  end
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
   },
+
   {
     "williamboman/mason-lspconfig.nvim",
-    config=function()
-      require("mason-lspconfig").setup({
-        ensure_installed={"lua_ls","pylsp","clangd","ts_ls","jsonls"}
-      })
-    end
+    lazy = false,
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "pylsp",
+        "clangd",
+        "ts_ls",
+        "jsonls",
+      },
+      automatic_installation = true,
+    },
   },
+
   {
     "neovim/nvim-lspconfig",
-    config=function()
-      vim.lsp.config("lua_ls",{})
-      vim.lsp.config("pylsp",{})
-      vim.lsp.config("clangd",{})
-      vim.lsp.config("ts_ls",{})
-      vim.lsp.config("jsonls",{})
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+      end
 
-      vim.keymap.set('n','K',vim.lsp.buf.hover,{})
-      vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
-      vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{})
-    end
-  }
+      -- Lua
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
 
+      -- Python
+      vim.lsp.config("pylsp", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- C/C++
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- TypeScript / JavaScript (NEW NAME)
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- JSON
+      vim.lsp.config("jsonls", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+      --linter for python
+      vim.lsp.config("ruff",{})
+    end,
+  },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua, --formatter for lua
         null_ls.builtins.formatting.black, --formatter for python
-        null_ls.builtins.diagnostics.ruff, --linter for python
+
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
